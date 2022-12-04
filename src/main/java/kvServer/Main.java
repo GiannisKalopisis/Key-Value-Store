1472cf89ea3938f6572b918018c852e4b8e0f034
Giannis Kalopisis <gianniskalopisis@gmail.com>
2022-12-04 15:34:27 +0200

    Server side (read ip and port)

--- src/main/java/kvServer/Main.java
+++ src/main/java/kvServer/Main.java
@@ -1,2 +1,16 @@
-package kvServer;public class Main {
+package kvServer;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Client side of Key-Value Database!");
+
+        ParametersController parametersController = new ParametersController();
+        parametersController.readParameters(args);
+
+
+
+
+    }
 }
--- src/main/java/kvServer/ParametersController.java
+++ src/main/java/kvServer/ParametersController.java
@@ -1,4 +1,43 @@
 package kvServer;
 
 public class ParametersController {
+
+    private String ipAddress;
+    private String port;
+
+
+    public void readParameters(String [] args){
+        if (!hasCorrectNumOfParameters(args.length)) {
+            throw new RuntimeException("Error at number of parameters. The correct number of arguments is 4. " +
+                    "You gave: " + args.length);
+        }
+        assignParameters(args);
+    }
+
+    private boolean hasCorrectNumOfParameters(int argsLength) {
+        return argsLength == 4;
+    }
+
+    private void assignParameters(String [] args) {
+        for (int i = 0; i < args.length; i += 2) {
+            switch (args[i]) {
+                case "-a":
+                    ipAddress = args[i + 1];
+                    break;
+                case "-p":
+                    port = args[i + 1];
+                    break;
+                default:
+                    throw new RuntimeException("Wrong parameter flag: " + args[i]);
+            }
+        }
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public String getPort() {
+        return port;
+    }
 }
