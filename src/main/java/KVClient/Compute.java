4d12fad82901aa86bcf08b64c02e344e51f0cfd1
Giannis Kalopisis <gianniskalopisis@gmail.com>
2022-12-12 15:15:19 +0200

    Check the name of variables

--- src/main/java/KVClient/Compute.java
+++ src/main/java/KVClient/Compute.java
@@ -1,14 +1,12 @@
 package KVClient;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Compute {
 
     private final String request;
     private String mathExpression;
-    private final ArrayList<String[]> queriesData;  // <varName, <query, value>>
+    private final ArrayList<String[]> queriesData;  // [varName, query, value]
 
     public Compute(String request) {
         this.request = request;
@@ -20,23 +18,60 @@ public class Compute {
         this.mathExpression = querySplittedAtWhere[0].replaceAll(" ","");
     }
 
-    public void exportQueries() {
+    public boolean exportQueries() {
         String[] querySplittedAtWhere = request.split(" ",2)[1].split("WHERE", 2);
         String queryParts = querySplittedAtWhere[1].trim();
         String[] queries = queryParts.split("AND");
         for (int i = 0; i < queries.length; i++) {
             queries[i] = queries[i].trim();
-            storeQuery(queries[i]);
+            if (!storeQuery(queries[i])) {
+                return false;
+            }
         }
+        return true;
     }
 
-    private void storeQuery(String query) {
+    private boolean storeQuery(String query) {
         String[] var_query = query.split(" = ");
         String[] temp = new String[3];
-        temp[0] = var_query[0];
+        if (validVariableName(var_query[0])) {
+            temp[0] = var_query[0];
+        } else {
+            return false;
+        }
         temp[1] = var_query[1];
         temp[2] = null;
         this.queriesData.add(temp);
+        return true;
+    }
+
+    private boolean validVariableName(String varName) {
+        switch (varName) {
+            case "l":
+            case "c":
+            case "s":
+            case "t":
+            case "a":
+            case "n":
+            case "i":
+            case "o":
+            case "g":
+            case "lo":
+            case "og":
+            case "log":
+            case "co":
+            case "os":
+            case "cos":
+            case "si":
+            case "in":
+            case "sin":
+            case "ta":
+            case "an":
+            case "tan":
+                return false;
+            default:
+                return true;
+        }
     }
 
     public String sendQueries(ArrayList<SocketStructure> sockets) {
@@ -47,7 +82,6 @@ public class Compute {
                 socket.writeToSocket(query[1]);
                 serverResponse = socket.readFromSocket();
                 if (!serverResponse.equals("NOT FOUND")) {
-                    System.out.println("response: " + serverResponse);
                     if (isNumber(serverResponse)) {
                         query[2] = serverResponse;
                         break;
@@ -84,49 +118,11 @@ public class Compute {
         }
     }
 
-    public String getMathExpression() {
-        return mathExpression;
-    }
-
     public String replaceVarsWithVals() {
         String newMathExpression = mathExpression;
         for (String[] query : queriesData) {
             newMathExpression = newMathExpression.replaceAll(query[0], query[2]);
         }
         return newMathExpression;
-    }
-
-    public String[] getVariablesNames() {
-        String[] vars = new String[queriesData.size()];
-        for (int i = 0; i < queriesData.size(); i++) {
-            vars[i] = queriesData.get(i)[0];
-        }
-        return vars;
-    }
-
-    public Map<String, Double> getVariablesValue() {
-        Map<String, Double> values = new HashMap<>();
-        for (int i = 0; i < queriesData.size(); i++) {
-            values.put(queriesData.get(i)[0],stringToDouble(queriesData.get(i)[2]));
-        }
-        return values;
-    }
-
-    public String reconstructMathExpression() {
-        StringBuilder newMathExpression = new StringBuilder();
-        for (int i = 0; i < queriesData.size(); i++) {
-            newMathExpression.append(queriesData.get(i)[0]).append("=").append(queriesData.get(i)[2]).append("; ");
-        }
-        newMathExpression.append(this.mathExpression).append(";");
-        return newMathExpression.toString();
-    }
-
-    private double stringToDouble(String value) {
-        try {
-            return Double.parseDouble(value);
-        } catch (NumberFormatException e) {
-            System.out.println("Couldn't convert " + value + " to double. Convert it to 0.0");
-            return 0.00;
-        }
     }
 }
