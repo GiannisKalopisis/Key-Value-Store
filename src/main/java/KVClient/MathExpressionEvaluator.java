28868a1aab8ad553dc7a7277bdf637c0808fc904
Giannis Kalopisis <gianniskalopisis@gmail.com>
2022-12-12 15:16:49 +0200

    Reformat code and comments

--- src/main/java/KVClient/MathExpressionEvaluator.java
+++ src/main/java/KVClient/MathExpressionEvaluator.java
@@ -8,61 +8,29 @@ public class MathExpressionEvaluator {
     public Double evaluate(String expression) {
         char[] tokens = expression.toCharArray();
 
-        // Stack for numbers: 'values'
         Stack<Double> values = new Stack<>();
-
-        // Stack for Operators: 'ops'
         Stack<Character> ops = new Stack<>();
 
         for (int i = 0; i < tokens.length; i++) {
-
-            // Current token is a
-            // whitespace, skip it
-            if (tokens[i] == ' ')
-                continue;
-
-            // Current token is a number,
-            // push it to stack for numbers
             if ((tokens[i] >= '0' && tokens[i] <= '9') || tokens[i] == '.') {
                 StringBuilder tempNumber = new StringBuilder();
-
-                // There may be more than one
-                // digits in number
                 while (i < tokens.length && ((tokens[i] >= '0' && tokens[i] <= '9') || tokens[i] == '.')) {
                     tempNumber.append(tokens[i++]);
                 }
-
                 values.push(Double.parseDouble(tempNumber.toString()));
-
-                // right now the i points to
-                // the character next to the digit,
-                // since the for loop also increases
-                // the i, we would skip one
-                //  token position; we need to
-                // decrease the value of i by 1 to
-                // correct the offset.
                 i--;
-            }
-            // Current token is an opening brace,
-            // push it to 'ops'
-            else if (tokens[i] == '(') {
+            } else if (tokens[i] == '(') {
                 ops.push(tokens[i]);
-            }
-            // Closing brace encountered,
-            // solve entire brace
-            else if (tokens[i] == ')') {
+            } else if (tokens[i] == ')') {
+                // solve entire brace
                 while (ops.peek() != '(') {
                     values.push(applyOp(ops.pop(), values.pop(), values.pop()));
                 }
                 ops.pop();
-                // CHECK IF NEXT OPS IS LOG/SIN/TAN/COS
-                // AND APPLY VALUE THERE
                 if (ops.peek() == 'l' || ops.peek() == 'c' || ops.peek() == 's' || ops.peek() == 't') {
                     values.push(calcFunction(ops.pop(), values.pop()));
                 }
-            }
-            // Current token is an operator.
-            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
+            } else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
                 // While top of 'ops' has same
                 // or greater precedence to current
                 // token, which is an operator.
@@ -71,8 +39,6 @@ public class MathExpressionEvaluator {
                 while (!ops.empty() && hasPrecedence(tokens[i], ops.peek())) {
                     values.push(applyOp(ops.pop(), values.pop(), values.pop()));
                 }
-
-                // Push current token to 'ops'.
                 ops.push(tokens[i]);
             }
             else if (tokens[i] == 'l' || tokens[i] == 'c' || tokens[i] == 's' || tokens[i] == 't') {
@@ -81,15 +47,11 @@ public class MathExpressionEvaluator {
             }
         }
 
-        // Entire expression has been
-        // parsed at this point, apply remaining
-        // ops to remaining values
+        // apply remaining ops to remaining values
         while (!ops.empty()) {
             values.push(applyOp(ops.pop(), values.pop(), values.pop()));
         }
 
-        // Top of 'values' contains
-        // result, return it
         return values.pop();
     }
 
@@ -108,18 +70,13 @@ public class MathExpressionEvaluator {
         }
     }
 
-    // Returns true if 'op2' has higher
-    // or same precedence as 'op1',
-    // otherwise returns false.
+    // Returns true if 'op2' has higher or same precedence as 'op1'
     private static boolean hasPrecedence(char op1, char op2) {
         if (op2 == '(' || op2 == ')')
             return false;
         return (op1 != '*' && op1 != '/') || (op2 != '+' && op2 != '-');
     }
 
-    // A utility method to apply an
-    // operator 'op' on operands 'a'
-    // and 'b'. Return the result.
     private static Double applyOp(char op, Double b, Double a) {
         switch (op) {
             case '+':
@@ -129,8 +86,9 @@ public class MathExpressionEvaluator {
             case '*':
                 return a * b;
             case '/':
-                if (b == 0) {
-                    throw new UnsupportedOperationException("Cannot divide by zero");
+                if (b == 0.0) {
+                    System.err.println("Cannot divide by zero. Convert it to one.");
+                    b = 1.0;
                 }
                 return a / b;
         }
